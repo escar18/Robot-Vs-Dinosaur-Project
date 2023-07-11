837fa25cd82a19042f7dbcee2484b28f19631b73
escar18 <131913910+escar18@users.noreply.github.com>
2023-07-11 16:43:04 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+from robot import Robot
+from dinosaur import Dinosaur
+from weapon import Weapon
+from battlefield import Battlefield
+
+def main():
+    # Create instances of Robot, Dinosaur, and Weapon
+    weapon = Weapon("Sword", 20)
+    robot = Robot("Robo1", 100, weapon)
+    dinosaur = Dinosaur("Dino1", 150, 30)
+
+    # Create an instance of Battlefield
+    battlefield = Battlefield(robot, dinosaur)
+
+    # Run the game
+    battlefield.run_game()
+
+if __name__ == '__main__':
+    main()
---
+++ robot.py
@@ -0,0 +1,10 @@
+
+
+class Robot:
+    def __init__(self, name, health, active_weapon):
+        self.name = name
+        self.health = health
+        self.active_weapon = active_weapon
+    
+    def attack(self, dinosaur):
+        dinosaur.health -= self.active_weapon.attack_power
